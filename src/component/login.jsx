63d70dd098ec59d17fa81cf017db011f44e2bab3
mukul0000 <mukulrajput9675@gmail.com>
2021-11-29 15:19:11 +0530

    login

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -4,8 +4,22 @@ export const Login = () =>{
 
     return (
         <>
-
-        <div>working</div>
+         <form>
+         <br/>
+         <br/>
+            <div><h1>Login page</h1></div>
+             <input placeholder = "user name"></input>
+             <br/>
+             <input placeholder = "email" type = "email"></input>
+             <br/>
+             <input placeholder = "mobile no" type = "number"></input>
+             <br/>
+             <input placeholder = "password" type = "password"></input>
+             <br/>
+             <input placeholder = "submit" type = "submit"></input>
+             
+         </form>
+        
         </>
     )
 
